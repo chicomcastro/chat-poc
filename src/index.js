4f5f9b5bcea995b8819b7c5dfd53c561ad87fd23
Francisco Castro <franciscommcastro@gmail.com>
2021-12-05 22:55:16 -0300

    Fix index.html path to serve on / route

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const io = new Server(server);
 
 // Express routes
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}\\index.html`);
+  res.sendFile(`${__dirname}/index.html`);
 });
 
 // Socket.io-related logic
